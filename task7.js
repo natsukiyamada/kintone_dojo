4d4ce6962c4d002d003a8ebb26e55b533b44f8c3
natsukiyamada <natsuking.smile09@gmail.com>
2022-07-05 17:51:07 +0900

    fix js ・if文の関数化

--- task7.js
+++ task7.js
@@ -15,25 +15,33 @@
 	];
 
 	kintone.events.on(targetFieldCode,(event)=>{
-		let today = new Date();
-		today = dateFns.format(today, 'YYYYMMDD') 
+		
+		const today = dateFns.format(new Date(), 'YYYYMMDD') 
 
 		//管理番号の値があるなら管理番号を、空なら空をManegementNumberに挿入（次行のselectedProductも同じ）
-		const ManagementNumber = (event.record.管理番号.value) ? event.record.管理番号.value:"";
-		let selectedProduct = (event.record.サイボウズ製品.value) ? event.record.サイボウズ製品.value:"";
+		const ManagementNumber = event.record.管理番号.value ? event.record.管理番号.value : "";
 	
 		//製品名=>製品の略称に変換
-    if (selectedProduct === 'kintone') {
-			selectedProduct = 'KN'
-		}else if(selectedProduct === 'Garoon'){
-			selectedProduct = 'GR';		
-		}else if(selectedProduct === 'サイボウズ Office'){
-			selectedProduct = 'OF';
-		}else if(selectedProduct === 'Mailwise'){
-			selectedProduct = 'MW';
-		}
+		let selectedProduct = "";
+		const getSelectedProductName = (tableValue) => {
+			if (tableValue === 'kintone') {
+					return selectedProduct = 'KN'
+				} 
+				if(tableValue === 'Garoon'){
+					return selectedProduct = 'GR';		
+				}
+				if(tableValue === 'サイボウズ Office'){
+					return selectedProduct = 'OF';
+				}
+				if(tableValue === 'Mailwise'){
+					return selectedProduct = 'MW';
+				}
+				return selectedProduct;
+		  }
+
+		selectedProduct = getSelectedProductName(event.record.サイボウズ製品.value);
+    
 		event.record.重複禁止項目_文字列.value = `${today}-${selectedProduct}-${ManagementNumber}`;
 		return event;
 	})
-
 })();
