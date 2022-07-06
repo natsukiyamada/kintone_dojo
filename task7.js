e95ba9ebcc4b658d7446c8225d51c835cec0d0db
natsukiyamada <natsuking.smile09@gmail.com>
2022-07-06 14:32:39 +0900

    イベント処理の共通化

--- task7.js
+++ task7.js
@@ -1,23 +1,18 @@
 (() => {
 	'use strict';
 	
-	kintone.events.on(['app.record.create.show','app.record.edit.show'], (event) => {
-		event.record.重複禁止項目_文字列.disabled = true;
-		return event;
-	});
-
 	const targetFieldCode = [
 	  'app.record.create.show',
 	  'app.record.edit.show',
-		'app.record.create.change.サイボウズ製品',
-		'app.record.create.change.管理番号',
+	  'app.record.create.change.サイボウズ製品',
+	  'app.record.create.change.管理番号',
 		'app.record.edit.change.サイボウズ製品',
 		'app.record.edit.change.管理番号',
 	];
 
-
 	kintone.events.on(targetFieldCode,(event)=>{
-		
+		event.record.重複禁止項目_文字列.disabled = true;
+
 		const today = dateFns.format(new Date(), 'YYYYMMDD') 
 		const managementNumber = event.record.管理番号.value ? event.record.管理番号.value : "";
 	
