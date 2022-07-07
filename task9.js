64006f0c4f29f257ff4aafd883b01e7dabdf50f0
natsukiyamada <natsuking.smile09@gmail.com>
2022-07-07 11:46:55 +0900

    クエリで重複レコードを取得するようにコード全体を変更

--- task9.js
+++ task9.js
@@ -1,30 +1,31 @@
 (() => {
 	'use strict';
-
-	const httpsRequestBody = {
-		"app":kintone.app.getId(),
-		"fields":["重複禁止項目"]
-	}
-
 	kintone.events.on('app.record.create.submit', (event) => {
 		const targetRecord = event.record.重複禁止項目.value;
+		const query = '重複禁止項目 = "' + targetRecord + '"';
+	
+		const httpsRequestBody = {
+			"app":kintone.app.getId(),
+			"query": query
+		}
+		
 		return kintone.api(kintone.api.url('/k/v1/records.json', true), 'GET', httpsRequestBody).then((resp) => {
+		
+			const handleDuplication = () => {
+				const result = window.confirm('レコードが重複しています。このまま保存しますか？');
+			  return result ? true:false;
+			}
 			
-			resp.records.forEach((respElem) => {
-				const refereceRecord = respElem.重複禁止項目.value
-				
-				if (refereceRecord === targetRecord) {
-					const result = window.confirm('レコードが重複しています。このまま保存しますか？');
-					if (!result) {
-						event.error = '処理を中断しました。'
-					}
-				}
-			});
+			if (resp.records.length !== 0) {
+				return handleDuplication() ? event : false;
+			} 
 
-			return event;
+			if (resp.records.length === 0) {
+				return event;
+			}
+
 		}).catch((error) =>{
 			console.log(error)
 		});
-		
 	});
 })();
